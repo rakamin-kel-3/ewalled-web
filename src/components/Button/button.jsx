d46c2dde8b45819d6aa21f128b99277fa112b279
kobulwidodo <fadhilhan01@gmail.com>
2025-03-23 14:05:19 +0700

    feat: add onclick styling

--- src/components/Button/button.jsx
+++ src/components/Button/button.jsx
@@ -6,7 +6,7 @@ const Button = ({ onClick, label, classname }) => {
     <button
       className={classNames(
         classname,
-        "bg-[#0061FF] px-5 py-2 rounded-md text-white font-bold"
+        "bg-[#0061FF] px-5 py-2 rounded-md text-white font-bold active:bg-[#0011ff]"
       )}
       onClick={onClick}
     >
