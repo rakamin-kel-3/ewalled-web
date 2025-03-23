cda547a0a8e49759348e16b5515e4a43d1015384
kobulwidodo <fadhilhan01@gmail.com>
2025-03-23 14:05:33 +0700

    refactor: dynamic rounded style

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,12 +1,20 @@
 import classNames from "classnames";
 import React from "react";
 
-const Input = ({ type, name, value, onChange, placeholder, classname }) => {
+const Input = ({
+  type,
+  name,
+  value,
+  onChange,
+  placeholder,
+  classname,
+  rounded = "md",
+}) => {
   return (
     <input
       className={classNames(
         classname,
-        "rounded-md px-5 py-4 bg-[#FAFBFD] text-black w-full text-md placeholder:font-semibold placeholder:text-black"
+        `rounded-${rounded} px-5 py-4 bg-[#FAFBFD] text-black w-full text-md placeholder:font-semibold placeholder:text-black`
       )}
       type={type}
       name={name}
