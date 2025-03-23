a7be1b31a3f069481777caffd17c7809cbdafa91
kobulwidodo <fadhilhan01@gmail.com>
2025-03-23 12:55:14 +0700

    fix: responsive navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -6,14 +6,20 @@ import {
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import classNames from "classnames";
 import React from "react";
+import { Link } from "react-router";
 import logo from "../../assets/logo.svg";
+import sunLight from "../../assets/sun-light.svg";
 
-const navigation = [{ name: "Home", href: "#", current: true }];
+const navigation = [
+  { name: "Dashboard", href: "/", current: true },
+  { name: "Transfer", href: "/trasnfer", current: false },
+  { name: "Topup", href: "/topup", current: false },
+];
 
 export const Navbar = () => {
   return (
     <Disclosure as="nav" className="bg-white">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-5xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             {/* Mobile menu button*/}
@@ -35,27 +41,29 @@ export const Navbar = () => {
               <img alt="Your Company" src={logo} className="h-8 w-auto" />
             </div>
           </div>
-          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <div className="flex space-x-10">
+          <div className="hidden sm:block absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            {/* Profile dropdown */}
+            <div className="flex lg:space-x-7 md:space-x-4 space-x-1 items-center">
               {navigation.map((item) => (
-                <a
+                <Link
                   key={item.name}
-                  href={item.href}
+                  to={item.href}
                   aria-current={item.current ? "page" : undefined}
                   className={classNames(
                     item.current
                       ? "text-[#0061FF]"
-                      : "text-black hover:bg-gray-700 hover:text-white",
+                      : "text-black hover:text-[#0061FF",
                     "rounded-md px-3 py-2 text-sm font-medium"
                   )}
                 >
                   {item.name}
-                </a>
+                </Link>
               ))}
-              <button className="text-black hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
+              <button className="text-black hover:text-[#0061FF] rounded-md px-3 py-2 text-sm font-medium">
                 Sign Out
               </button>
-              <div className="border-1 h-5 my-auto"></div>
+              <div className="border-1 h-7 border-[#B3B3B3] my-auto"></div>
+              <img src={sunLight} className="h-6 w-auto" />
             </div>
           </div>
         </div>
@@ -72,7 +80,7 @@ export const Navbar = () => {
               className={classNames(
                 item.current
                   ? "text-[#0061FF]"
-                  : "text-black hover:bg-gray-700 hover:text-white",
+                  : "text-black hover:text-[#0061FF]",
                 "block rounded-md px-3 py-2 text-base font-medium"
               )}
             >
