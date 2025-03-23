adb1d4fd5a6a8c18d2caefd3e3f1f6857d5f6d9e
kobulwidodo <fadhilhan01@gmail.com>
2025-03-23 10:22:29 +0700

    feat: create register page

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 const Routers = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/" element={<Home />} />
         {/* <Route path="/pokedetail/:id" element={<PokeDetail />} /> */}
       </Routes>
