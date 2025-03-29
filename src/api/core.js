7d3d2e8a412965ccefeba57ea3df08c9d8a0f874
Rakhmad Giffari Nurfadhilah <53964878+kobulwidodo@users.noreply.github.com>
2025-03-29 23:23:29 +0700

    feat: update api link

--- src/api/core.js
+++ src/api/core.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://savings-spell-md-trick.trycloudflare.com";
+const BASE_URL = "https://fulusku-api.kobulwidodo.my.id";
 
 export default axios.create({
   baseURL: BASE_URL,
